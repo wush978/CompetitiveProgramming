f98285f83940d27e372b58c6ddba6bbc2bd27e2b
Wush Wu <wush978@gmail.com>
2018-09-30 14:10:47 +0800

    add helper functions for suffixArray and getLCP

--- suffixArray.cpp
+++ suffixArray.cpp
@@ -5,10 +5,14 @@
 int main() {
   std::string str;
   if (!std::getline(std::cin, str)) return 1;
-  std::vector<char> input(str.begin(), str.end());
-  auto result = sortCyclicShifts<char, 256>(input);
+  auto result(suffixArray(str));
   for(std::size_t i = 0;i < result.size();i++) {
     std::cout << (result[i]) << " ";
+  }
+  std::cout << std::endl;
+  auto lcp(getLCP(str, result));
+  for(std::size_t i = 0;i < lcp.size();i++) {
+    std::cout << (lcp[i]) << " ";
   }
   std::cout << std::endl;
   return 0;
--- suffixArray.hpp
+++ suffixArray.hpp
@@ -1,19 +1,21 @@
 #include <vector>
 #include <cmath>
 #include <climits>
+#include <string>
 
 /**
  * @param T The typename of the sorted objects
  * @param k The number of classes of the sorted objects
  * Reference: https://cp-algorithms.com/string/suffix-array.html
  */
-template<typename T, int K>
-std::vector<std::size_t> sortCyclicShifts(const std::vector<T>& input, int (*key)(const T&)) {
+template<typename V, std::size_t K>
+std::vector<std::size_t> sortCyclicShifts(const V& input, std::size_t (*key)(const typename V::value_type&)) {
+  typedef typename V::value_type T;
   if (input.size() == 0) return std::vector<std::size_t>();
   const std::size_t n = input.size();
 
   std::vector<std::size_t> p(n, 0);
-  std::vector<int> c(n, 0);
+  std::vector<std::size_t> c(n, 0);
 
   // counting sort
   std::vector<std::size_t> count(K, 0);
@@ -23,11 +25,11 @@ std::vector<std::size_t> sortCyclicShifts(const std::vector<T>& input, int (*key
   std::size_t csum = 0, tmp;
   for(auto i = count.begin();i != count.end();i++) {
     tmp = *i;
-    *i += csum;
+    *i = csum;
     csum += tmp;
   }
   for(std::size_t i = 0;i < n;i++) {
-    int j = key(input[i]);
+    std::size_t j = key(input[i]);
     p[count[j]] = i;
     c[i] = j;
     count[j] += 1;
@@ -38,7 +40,6 @@ std::vector<std::size_t> sortCyclicShifts(const std::vector<T>& input, int (*key
   std::size_t shift = 1;
   while(step <= stepBound) {
     // counting sort i - 2^k
-    std::vector<int> ctmp(n, 0);
     std::fill(count.begin(), count.end(), 0);
     for(std::size_t i = 0;i < shift;i++) {
       count[c[i + (n - shift)]] += 1;
@@ -46,36 +47,81 @@ std::vector<std::size_t> sortCyclicShifts(const std::vector<T>& input, int (*key
     for(std::size_t i = shift;i < n;i++) {
       count[c[i - shift]] += 1;
     }
+    // reset count / c
+    std::vector<std::size_t> cmap(count.size(), 0);
+    std::size_t cmapSize = 0;
+    std::vector<std::size_t> countTmp;
+    for(std::size_t i = 0;i < count.size();i++) {
+        if (count[i] > 0) {
+            cmap[i] = cmapSize++;
+            countTmp.push_back(count[i]);
+        }
+    }
+    std::swap(count, countTmp);
+    std::vector<std::size_t> ctmp(n);
     csum = 0;
     for(auto i = count.begin();i != count.end();i++) {
       tmp = *i;
-      *i += csum;
+      *i = csum;
       csum += tmp;
     }
-    for(std::size_t i = 0;i < shift;i++) {
-      int j = c[i + (n - shift)];
-      p[count[j]] = i;
-      ctmp[i] = j;
-      count[j] += 1;
+    std::size_t countSize = count.size();
+    std::vector<std::size_t> ptmp(n);
+    for(auto i = p.begin();i != p.end();i++) {
+      std::size_t shifted = (*i < shift ? *i + n - shift : *i - shift);
+      std::size_t shiftedC = cmap[c[shifted]];
+      ptmp[count[shiftedC]] = shifted;
+      ctmp[shifted] = shiftedC * cmapSize + cmap[c[*i]];
+      if (ctmp[shifted] + 1 > countSize) countSize = ctmp[shifted] + 1;
+      count[shiftedC] += 1;
     }
-    for(std::size_t i = shift;i < n;i++) {
-      int j = c[i - shift];
-      p[count[j]] = i;
-      ctmp[i] = j;
-      count[j] += 1;
-    }
+    count.resize(countSize);
     shift = (shift * 2) % n;
-    step++;
     std::swap(c, ctmp);
+    std::swap(p, ptmp);
+    step++;
   }
   return p;
 }
 
-template<typename T, int K>
-std::vector<std::size_t> sortCyclicShifts(const std::vector<T>& input) {
-  typedef int (*KeyType)(const T&);
+template<typename V, std::size_t K>
+std::vector<std::size_t> sortCyclicShifts(const V& input) {
+  typedef typename V::value_type T;
+  typedef std::size_t (*KeyType)(const T&);
   KeyType key = [](const T& t) {
-    return ((int) t);
+    return ((std::size_t) t);
   };
-  return sortCyclicShifts<T, K>(input, key);
+  return sortCyclicShifts<V, K>(input, key);
+}
+
+std::vector<std::size_t> suffixArray(std::string& input) {
+  input.push_back('$');
+  auto result(sortCyclicShifts<std::string, 256>(input));
+  result.erase(result.begin());
+  input.pop_back();
+  return result;
+}
+
+std::vector<std::size_t> getLCP(const std::string& input, const std::vector<std::size_t>& sa) {
+  std::size_t n = input.size();
+  std::vector<std::size_t> rank(n, 0);
+  for(std::size_t i = 0;i < sa.size();i++) {
+    rank[sa[i]] = i;
+  }
+
+  std::size_t k;
+  std::vector<std::size_t> result(n - 1, 0);
+  for(std::size_t i = 0;i < n;i++) {
+    if (rank[i] == n - 1) {
+      k = 0;
+      continue;
+    }
+    std::size_t j = sa[rank[i] + 1];
+    while(i + k < n && j + k < n && input[i + k] == input[j + k]) {
+      k++;
+    }
+    result[rank[i]] = k;
+    if (k > 0) k--;
+  }
+  return result;
 }
