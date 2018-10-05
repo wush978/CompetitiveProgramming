9ed1ac8778a78e4e86a4dd50c5003d551d445073
Wush Wu <wush978@gmail.com>
2018-10-06 02:33:01 +0800

    suffix array is O(nlogn) now

--- Tandem.cpp
+++ Tandem.cpp
@@ -20,12 +20,16 @@ std::vector<std::size_t> sortCyclicShifts(const std::string& x) {
   for(std::size_t i = 0;i < x.size();i++) {
     count[static_cast<unsigned char>(x[i])] += 1;
   }
-  for(std::size_t i = 1;i < count.size();i++) {
-    count[i] += count[i - 1];
+  std::size_t csum = 0, tmp;
+  for(std::size_t i = 0;i < count.size();i++) {
+    tmp = count[i];
+    count[i] = csum;
+    csum += tmp;
   }
   for(std::size_t i = 0;i < n;i++) {
     unsigned char j = static_cast<unsigned char>(x[i]);
     p[count[j]] = i;
+    count[j] += 1;
   }
   std::size_t cValue = 0;
   c[p[0]] = cValue;
@@ -37,7 +41,7 @@ std::vector<std::size_t> sortCyclicShifts(const std::string& x) {
   std::size_t step = 1, stepBound = std::ceil(std::log2((double) n)), shift = 1;
   std::vector<std::size_t> ctmp(n, 0), ptmp(n, 0);
   while(step <= stepBound) {
-    count.resize(cValue);
+    count.resize(cValue + 1);
     std::fill(count.begin(), count.end(), 0);
     for(std::size_t i = 0;i < shift;i++) {
       count[c[i + (n - shift)]] += 1;
@@ -45,8 +49,11 @@ std::vector<std::size_t> sortCyclicShifts(const std::string& x) {
     for(std::size_t i = shift;i < n;i++) {
       count[c[i - shift]] += 1;
     }
-    for(std::size_t i = 1;i < count.size();i++) {
-      count[i] += count[i-1];
+    csum = 0;
+    for(std::size_t i = 0;i < count.size();i++) {
+      tmp = count[i];
+      count[i] = csum;
+      csum += tmp;
     }
     std::fill(ptmp.begin(), ptmp.end(), 0);
     std::fill(ctmp.begin(), ctmp.end(), 0);
@@ -54,8 +61,27 @@ std::vector<std::size_t> sortCyclicShifts(const std::string& x) {
       std::size_t shifted = (i < shift ? i + n - shift : i - shift);
       std::size_t shiftedC = c[shifted];
       ptmp[count[shiftedC]] = shifted;
+      count[shiftedC] += 1;
     }
-
+    cValue = 0;
+    ctmp[ptmp[0]] = cValue;
+    for(std::size_t i = 1;i < n;i++) {
+      std::size_t j = ptmp[i];
+      std::size_t jShifted = j + shift;
+      if (jShifted >= n) jShifted -= n;
+      std::size_t c1 = c[j], c2 = c[jShifted];
+      // c[jShifted] , c[j]
+      j = ptmp[i - 1];
+      jShifted = j + shift;
+      if (jShifted >= n) jShifted -= n;
+      std::size_t d1 = c[j], d2 = c[jShifted];
+      if (c1 > d1 || (c1 == d1 && c2 > d2)) {
+        cValue++;
+      }
+      if (c1 < d1) throw std::logic_error("c1 < d1");
+      if (c1 == d1 && c2 < d2) throw std::logic_error("c2 < d2");
+      ctmp[ptmp[i]] = cValue;
+    }
     shift = (shift * 2) % n;
     std::swap(c, ctmp);
     std::swap(p, ptmp);
@@ -141,11 +167,6 @@ int main() {
     return 0;
   }
   auto sa(suffixArray(str));
-  for(auto i : sa) {
-    std::cout << i << " ";
-  }
-  std::cout << std::endl;
-  return 0;
   // inverse index of suffix array
   std::vector<std::size_t> rank(sa.size(), 0);
   for(std::size_t i = 0;i < sa.size();i++) {
@@ -153,7 +174,6 @@ int main() {
   }
 
   auto lcp(getLCP(str, sa));
-  return 0;
 /*
   for(auto i : sa) {
     std::cout << i << " ";
